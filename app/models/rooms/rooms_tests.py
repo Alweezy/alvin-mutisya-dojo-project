f2ea36a81ea071f842414dfdbbf416558fccce3c
Alweezy <alvinmutisya@gmail.com>
2017-05-15 16:47:47 +0300

    Remove redundant test case

--- app/models/rooms/rooms_tests.py
+++ app/models/rooms/rooms_tests.py
@@ -25,31 +25,4 @@ class LivingSpaceTestCase(TestCase):
 
     def test_room_correct_room_capacity(self):
         self.python = LivingSpace('Python')
-        self.assertEqual(self.python.room_capacity, 4)from rooms import Room, Office,  LivingSpace
-from unittest import TestCase
-
-
-class RoomTestCase(TestCase):
-
-    def test_room_instance(self):
-        self.valhalla = Room('Valhalla', 'office', 6)
-        self.assertEqual(self.valhalla.room_name, 'Valhalla')
-
-
-class OfficeSpaceTestCase(TestCase):
-
-    def test_class_inherits_from_room(self):
-        self.assertTrue(issubclass(Office, Room))
-
-    def test_room_correct_room_capacity(self):
-        self.valhalla = Office('Valhalla')
-        self.assertEqual(self.valhalla.room_capacity, 6)
-
-
-class LivingSpaceTestCase(TestCase):
-    def test_class_inherits_from_room(self):
-        self.assertTrue(issubclass(LivingSpace, Room))
-
-    def test_room_correct_room_capacity(self):
-        self.python = LivingSpace('Python')
         self.assertEqual(self.python.room_capacity, 4)
