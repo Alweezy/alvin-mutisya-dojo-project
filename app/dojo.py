fa50a4d9ab93155675d5a97f8a8d89e9e35db887
Alweezy <alvinmutisya@gmail.com>
2017-05-17 22:27:20 +0300

    Implement functionality to mee tests

--- app/dojo.py
+++ app/dojo.py
@@ -22,7 +22,7 @@ class Dojo(object):
         # return False if all rooms are full
         if len(available_rooms) < 1:
             return False
-        # choose a room fro the list of available rooms.
+        # choose a room from the list of available rooms.
         chosen_room = random.choice(available_rooms)
         return chosen_room.room_name
 
@@ -52,23 +52,21 @@ class Dojo(object):
         if occupation is 'Fellow':
             if self.person_name not in [person.fname + person.lname for person in self.fellows]:
                 person = Fellow(first_name, last_name, occupation, wants_accommodation)
-                print(first_name + last_name + ' has been added successfully!')
+                self.fellows.append(person)
+                print(first_name + ' ' + last_name + ' has been added successfully!')
                 # check if fellow wants accommodation, it is set to 'N' by default
                 accommodation = person.wants_accommodation
                 if accommodation is None:
                     # if a fellow does not want  accommodation then they get only office space
-                    self.fellows.append(person)
                     work_room = self.get_room(self.offices)
                     # if there is no available office space
                     if work_room:
                         for room in self.offices:
                             if room.room_name == work_room:
                                 room.occupants.append(person)
-                                # deduct room capacity to update
-                                room.room_capacity -= 1
-                                print('A ' + person.occupation + ' ' + person.fname + ' has been added to ' + work_room)
-                            else:
-                                print('All offices are occupied, add a new office')
+                                return'A ' + person.occupation + ' ' + person.fname + ' has been added to ' + work_room
+                    else:
+                        return 'All offices are occupied, add a new office'
                 else:
                     # if a fellow does not want  accommodation then they get only office space
                     work_room = self.get_room(self.offices)
@@ -78,8 +76,6 @@ class Dojo(object):
                         for room in self.offices:
                             if room.room_name == work_room:
                                 room.occupants.append(person)
-                                # update room capacity
-                                room.room_capacity -= 1
                                 print('A ' + person.occupation + ' ' + person.fname + ' has been added to ' + work_room)
                     else:
                         print('All offices are occupied, add a new office')
@@ -87,15 +83,46 @@ class Dojo(object):
                         for room in self.livingrooms:
                             if room.room_name == living_room:
                                 room.occupants.append(person)
-                                # update room capacity
-                                room.room_capacity -= 1
                                 print('A ' + person.occupation + ' ' + person.fname + ' has been added to ' + living_room)
                     else:
-                        print('All living rooms are occupied, add a new living room')
+                        return 'All living rooms are occupied, add a new living room'
             else:
                 print('A fellow with that name already exists')
         if occupation is 'Staff':
-            person = Staff(fname=first_name, lname=last_name, occupation=occupation)
-            self.all_people.append(person)
-            self.staff.append(person)
-            return 'A ' + person.occupation + ' ' + person.fname + person.lname + ' has been added successfully!'
+            if self.person_name not in [person.fname + person.lname for person in self.staff]:
+                person = Staff(first_name, last_name, occupation)
+                print(first_name + ' ' + last_name + ' has been added successfully!')
+                self.staff.append(person)
+                work_room = self.get_room(self.offices)
+                if work_room:
+                    for room in self.offices:
+                        if room.room_name == work_room:
+                            room.occupants.append(person)
+                            print('A ' + person.occupation + ' ' + person.fname + ' has been added to ' + work_room)
+                else:
+                    print('All offices are occupied, add a new office')
+            else:
+                print('A member of staff with that name already exists')
+
+
+
+
+
+
+
+dojo = Dojo()
+# print(dojo.create_room('Valhalla', 'office'))
+# print(dojo.create_room('Django', 'livingspace'))
+# print(dojo.create_room('Tango', 'livingspace'))
+# print(dojo.add_person('Agatha', 'Wairimu', 'Fellow'))
+# print(dojo.add_person('Agatha', 'Wairimu', 'Fellow'))
+print(dojo.add_person('Agath', 'Wairimu', 'Fellow', 'Y'))
+# print(dojo.add_person('Betty', 'Wairimu', 'Fellow'))
+# print(dojo.add_person('Betty', 'Wairimu', 'Fellow'))
+# print(dojo.add_person('Agatha', 'Wairimu', 'Fellow', 'Y'))
+# print(dojo.add_person('Agatha', 'Wairimu', 'Fellow', 'Y'))
+#
+# print(dojo.add_person('Betty', 'Wairimu', 'Fellow'))
+# print(dojo.add_person('Betty', 'Wairimu', 'Staff'))
+# print(dojo.add_person('Betty', 'Wairimu', 'Staff'))
+# print(dojo.fellows)
