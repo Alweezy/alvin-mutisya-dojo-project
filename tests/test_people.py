03072d942f8300c7ad0ad325a1165ca8771e0d33
Alweezy <alvinmutisya@gmail.com>
2017-06-02 11:49:52 +0300

    Add tests for id generation

--- tests/test_people.py
+++ tests/test_people.py
@@ -6,7 +6,7 @@ class PersonTestCases(TestCase):
     """Tests the functionality of the person parent class
     """
     def setUp(self):
-        """Passess an instance of class Person to all the methods in this class
+        """Passes an instance of class Person to all the methods in this class
         """
         self.person = Person('Oluwafemi', 'Sule', 'Fellow')
 
@@ -24,13 +24,19 @@ class FellowTestCases(TestCase):
     def test_person_name_is_correct(self):
         self.assertEqual(self.fellow.fname + ' ' + self.fellow.lname, 'Nadia Alexis')
 
+    def test_fellow_id_generation(self):
+        self.assertEqual(self.fellow.id, 'fel24')
+
 
 class StaffTestCases(TestCase):
     def setUp(self):
-        self.staff = Staff('Nadia', 'Alexis', 'Fellow')
+        self.staff = Staff('Nadia', 'Alexis', 'Staff')
 
     def test_if_inherits_from_Person(self):
         self.assertTrue(issubclass(Staff, Person))
 
     def test_full_name_is_correct(self):
         self.assertEqual(self.staff.fname + ' ' + self.staff.lname, 'Nadia Alexis')
+
+    def test_staff_id_generation(self):
+        self.assertEqual(self.staff.id, 'stf29')
