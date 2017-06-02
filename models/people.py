0c96c410b3d9f92141e6584decd4dcb2e94ddc06
Alweezy <alvinmutisya@gmail.com>
2017-06-02 12:07:29 +0300

    Implement generate_id functionality

--- models/people.py
+++ models/people.py
@@ -2,6 +2,10 @@ class Person(object):
     """Models the kind of people available at Andela,
     It forms the base class from which classes Fellow and Staff inherit"""
 
+    person_id = 0
+    staff_id = ''
+    fellow_id = ''
+
     def __init__(self, fname, lname, occupation):
         """Initializes the base class Person
         :param fname: A string denoting person's first name
@@ -13,13 +17,31 @@ class Person(object):
         self.occupation = occupation
         self.person_name = self.fname + self.fname
 
+    def generate_person_id(self):
+        """Generates a unique id for any person (Staff|Fellow) added to the system
+        :return: fellow_id|person_id
+        """
+        if self.occupation is 'Fellow':
+            Person.person_id += 1
+            fellow_id = 'fel' + str(Person.person_id)
+            return fellow_id
+        elif self.occupation is 'Staff':
+            Person.person_id += 1
+            staff_id = 'stf' + str(Person.person_id)
+            return staff_id
+        else:
+            return False
+
 
 class Fellow(Person):
     def __init__(self, fname, lname, occupation, wants_accommodation='N'):
         super(Fellow, self).__init__(fname, lname, occupation='Fellow')
         self.wants_accommodation = wants_accommodation
+        self.id = self.generate_person_id()
 
 
 class Staff(Person):
     def __init__(self, fname, lname, occupation):
         super(Staff, self).__init__(fname, lname, occupation='Staff')
+        self.id = self.generate_person_id()
+
