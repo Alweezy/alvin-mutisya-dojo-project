b51a5ab7a76b76c4922f7484097d73276080a63d
Alweezy <alvinmutisya@gmail.com>
2017-06-05 23:02:53 +0300

    Edit test's persons id to work with nosetests

--- tests/test_people.py
+++ tests/test_people.py
@@ -25,7 +25,7 @@ class FellowTestCases(TestCase):
         self.assertEqual(self.fellow.fname + ' ' + self.fellow.lname, 'Nadia Alexis')
 
     def test_fellow_id_generation(self):
-        self.assertEqual(self.fellow.id, 'fel29')
+        self.assertEqual(self.fellow.id, 'fel28')
 
 
 class StaffTestCases(TestCase):
@@ -39,4 +39,4 @@ class StaffTestCases(TestCase):
         self.assertEqual(self.staff.fname + ' ' + self.staff.lname, 'Nadia Alexis')
 
     def test_staff_id_generation(self):
-        self.assertEqual(self.staff.id, 'stf34')
+        self.assertEqual(self.staff.id, 'stf33')
