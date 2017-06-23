d61cc48b09fee51b2f4fe135caa14ec38462a757
Alweezy <alvinmutisya@gmail.com>
2017-06-23 18:07:32 +0300

    Edit table names

--- database/schema.py
+++ database/schema.py
@@ -16,20 +16,14 @@ class People(Base):
     first_name = Column(String(200))
     last_name = Column(String(200))
     occupation = Column(String(200))
-    wants_accommodation = Column(String(5))
-    office_allocated = Column(String(200))
-    living_space_allocated = Column(String(200))
+    wants_accommodation = Column(String(20), nullable=True)
 
-    def __init__(self, person_id, first_name, last_name, occupation,
-                 wants_accommodation, office_allocated,
-                 living_space_allocated):
+    def __init__(self, person_id, first_name, last_name, occupation, wants_accommodation):
         self.person_id = person_id
         self.first_name = first_name
         self.last_name = last_name
         self.occupation = occupation
         self.wants_accommodation = wants_accommodation
-        self.office_allocated = office_allocated
-        self.living_space_allocated = living_space_allocated
 
 
 class Rooms(Base):
@@ -40,11 +34,13 @@ class Rooms(Base):
     room_name = Column(String(200), nullable=False)
     room_type = Column(String(200), nullable=False)
     room_capacity = Column(Integer)
+    room_occupants = Column(String(2000), nullable=False)
 
-    def __init__(self, room_name, room_type, room_capacity):
+    def __init__(self, room_name, room_type, room_capacity, room_occupants):
         self.room_name = room_name
         self.room_type = room_type
         self.room_capacity = room_capacity
+        self.room_occupants = room_occupants
 
 
 class Unallocated(Base):
@@ -53,28 +49,30 @@ class Unallocated(Base):
     __tablename__ = 'unallocated'
     id = Column(Integer, primary_key=True)
     person_id = Column(String(200), nullable=False)
-    person_name = Column(String(200), nullable=False)
+    first_name = Column(String(200), nullable=False)
+    last_name = Column(String(200), nullable=False)
     occupation = Column(String(200), nullable=False)
     room_unallocated = Column(String(200), nullable=False)
 
-    def __init__(self, person_id,  person_name, occupation, room_unallocated):
+    def __init__(self, person_id,  first_name, last_name, occupation, room_unallocated):
         self.person_id = person_id
-        self.person_name = person_name
+        self.first_name = first_name
+        self.last_name = last_name
         self.occupation = occupation
         self.room_unallocated = room_unallocated
 
 
 class DataBaseConnection(object):
-    """Creates a template for db connection
+    """Creates a template for database connection
     """
 
     def __init__(self, db_name=None):
         self.db_name = db_name
         if self.db_name:
-            self.db_name = db_name + '.sqlite'
+            self.db_name = db_name + '.db'
         else:
-            self.db_name = 'default_amity_db.sqlite'
+            self.db_name = 'default_amity_db.db'
         self.engine = create_engine('sqlite:///' + self.db_name)
-        self.session = sessionmaker()
-        self.session.configure(bind=self.engine)
+        Base.metadata.bind = self.engine
         Base.metadata.create_all(self.engine)
+        self.Session = sessionmaker(bind=self.engine)
