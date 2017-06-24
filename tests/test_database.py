ac4b421c38f4a402d94ac8db07f534b2b5b1f955
Alweezy <alvinmutisya@gmail.com>
2017-06-24 17:53:01 +0300

    Edit path allocations.txt file

--- tests/test_database.py
+++ tests/test_database.py
@@ -9,13 +9,17 @@ sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 class TestSaveState(unittest.TestCase):
     def setUp(self):
         self.dojo = Dojo()
+        self.dojo.create_room('Hogwarts', 'Office')
+        self.dojo.load_people('models/allocation.txt')
 
     def test_database_created_successfully(self):
-        self.dojo.create_room('Hogwarts', 'Office')
-        self.dojo.load_people('allocation.txt')
         self.dojo.save_state('models/dojo_test_database')
         self.assertEqual(path.exists('models/dojo_test_database.db'), True)
 
+    def test_people_table_persist_correct(self):
+        self.dojo.all_people = self.dojo.fellows + self.dojo.staff
+        people = len(self.dojo.all_people)
+        self.assertEqual(7, people)
 
 class TestLoadState(unittest.TestCase):
         def setUp(self):
