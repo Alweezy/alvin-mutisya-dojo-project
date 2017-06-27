a59e7f9459e6c8c69ace01107ecbd03997191b9d
Alweezy <alvinmutisya@gmail.com>
2017-06-27 09:37:41 +0300

    Make base class Abstract

--- models/rooms.py
+++ models/rooms.py
@@ -1,9 +1,11 @@
 import sys
 from os import path
+from abc import ABCMeta, abstractmethod
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 
-class Room(object):
+class Room:
+    __metaclass__ = ABCMeta
     """Models the kind of rooms available at Andela,
         It forms the base class Room from which OfficeSpace and LivingRoom inherit"""
     def __init__(self, room_name, room_type, room_capacity):
@@ -17,14 +19,24 @@ class Room(object):
         self.room_capacity = room_capacity
         self.occupants = []
 
+    @abstractmethod
+    def __repr__(self):
+        return self.room_name
+
 
 class Office(Room):
 
     def __init__(self, room_name, room_type):
         super(Office, self).__init__(room_name, room_type, room_capacity=6)
 
+    def __repr__(self):
+        return self.room_name
+
 
 class LivingSpace(Room):
 
     def __init__(self, room_name, room_type):
         super(LivingSpace, self).__init__(room_name, room_type, room_capacity=4)
+
+    def __repr__(self):
+        return self.room_name
